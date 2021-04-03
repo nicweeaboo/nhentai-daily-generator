cb3b0c6af1c6eccaf29dda3c87a0e762991189fd
nicweeaboo <gregory.mayer1998@gmail.com>
2021-04-03 13:33:04 -0300

    hide shwo thumb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-import Rails from "@rails/ujs"
+//= require_self
+import Rails, { $ } from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
@@ -12,3 +12,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+document.addEventListener("DOMContentLoaded", ()=> {
+    const btn = document.querySelector("#btn")
+    const thumb = document.querySelector('#thumb');
+    function toggleBlur(event){
+      event.preventDefault();
+      thumb.classList.toggle('blur');
+    }
+    btn.addEventListener('click', toggleBlur);
+  })
